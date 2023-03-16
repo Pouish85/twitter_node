3ff29db7db8cef7352373cae50fa7ee9ecf198c9
Pouish85 <pouish.iphone@gmail.com>
2023-03-16 11:02:15 +0100

    feat: ajout de la route comment

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,14 @@ const tweetRoute = require("./tweet.routes");
 const { tweetList } = require('../controllers/tweet.controler');
 const userRoute = require('./user.routes');
 const authRoute = require("./auth.routes");
+const commentRoute = require("./comment.routes");
 const { ensureAuthenticated } = require('../config/security.config');
 
 
 router.use('/tweet', ensureAuthenticated, tweetRoute);
 router.use('/user', userRoute);
 router.use('/auth', authRoute);
+router.use('/comment',ensureAuthenticated, commentRoute);
 
 router.get('/', tweetList);
 
